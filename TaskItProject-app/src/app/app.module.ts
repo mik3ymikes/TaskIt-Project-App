e16a09fc3a5980bf68dab0e3c1d165247d4dc064
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-10-03 16:21:44 -0500

    Added CSS Grid

--- TaskItProject-app/src/app/app.module.ts
+++ TaskItProject-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
