cba7495a0f479d39665f24c60d74023b4dbfa283
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-10-19 14:05:43 -0500

    Finishing up landing page . Still need photo

--- TaskItProject-app/src/app/landing/landing.component.ts
+++ TaskItProject-app/src/app/landing/landing.component.ts
@@ -12,57 +12,13 @@ import { CdkDragDrop,moveItemInArray,transferArrayItem, CdkDrag, CdkDropList} fr
 
 export class LandingComponent {
 
-  tasks:any[]
-  inProg:any[]
-  done:any[]
-
-  noWork=true
-  // isItemsDisabled: boolean = true;
-
-
-
-  ngOnInit(): void{
-    this.tasks=[{title: "Clean Out Garage", due: "March 31st", priority: "High", status: null, description: null}]
-    this.inProg=[{title: "Clean Out Garage", due: "March 31st", priority: "High", status: null, description: null}]
-    this.done=[{title: "Clean Out Garage", due: "March 31st", priority: "High", status: null, description: null}]
-
-
-
-    // this.outputLocalStorageData()
-    // this.inProg=[]
-    // this.done=[]
-    // this.getVal()
-  }
-
-
-  // outputLocalStorageData(){
-  //   // const localStorageData = this.localStorageService.getData('savedData');
-  //   // console.log('Local Storage Data:', localStorageData);
-  //   console.log(this.tasks)
-
-  // }
-
-
-
-  drop(event: CdkDragDrop<string[]>) {
-
-
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-  } else {
-    transferArrayItem(
-      event.previousContainer.data,
-      event.container.data,
-      event.previousIndex,
-      event.currentIndex,
-      );
-    }
-
-  }
-
-
-
-
 
 }
+
+
+
+
+
+
+
 
