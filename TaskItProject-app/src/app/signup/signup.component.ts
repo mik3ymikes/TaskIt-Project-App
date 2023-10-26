4c9f2aebe68ffdb4e4406376f1e1e444603c2d3d
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-10-26 15:12:07 -0500

    error in console for some reason

--- TaskItProject-app/src/app/signup/signup.component.ts
+++ TaskItProject-app/src/app/signup/signup.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
+
 export class SignupComponent {
+  form:NgForm
 
+  onSubmit(form: NgForm){
 
-  onSubmit(){
-    
   }
 
 }
